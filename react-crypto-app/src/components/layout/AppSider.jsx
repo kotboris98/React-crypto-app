50b2165a4756dee3920b0f288c7ca8a8a0817b51
kotboris98 <borisvip169@gmail.com>
2025-01-31 00:54:54 +0300

    Initial commit

--- react-crypto-app/src/components/layout/AppSider.jsx
+++ react-crypto-app/src/components/layout/AppSider.jsx
@@ -1,13 +1,54 @@
-import { Layout, Card, Statistic } from 'antd';
+import { Layout, Card, Statistic, List, Typography, Spin } from 'antd';
 import { ArrowDownOutlined, ArrowUpOutlined } from '@ant-design/icons';
+import { useEffect, useState } from 'react';
+import { fakeFetchCrypto, fetchAssets } from '../../api';
+import { percentDifference } from '../../utils';
 
 const siderStyle = {
     padding: '1rem',
   };
 
+  const data = [
+    'Racing car sprays burning fuel into crowd.',
+    'Japanese princess to wed commoner.',
+    'Australian walks 100km after outback crash.',
+    'Man charged over missing wedding girl.',
+    'Los Angeles battles huge wildfires.',
+  ];
+
 export default function AppSider() {
+  const [loading, setLoading] = useState(false)
+  const [crypto, setCrypto] = useState([])
+  const [assets, setAssets] = useState([])
+  
+  useEffect(() => {
+    async function preload() {
+      setLoading(true)
+      const {result} = await fakeFetchCrypto()
+      const assets = await fetchAssets()
+
+      setAssets(assets.map(asset => {
+        const coin = result.find((c) => c.id === asset.id)
+        return {
+          grow: asset.price < coin.price,
+          growPercent: percentDifference(asset.price, coin.price),
+          totalAmount: asset.amount * coin.price,
+          totalProfit: asset.amount * coin.price - asset.amount * asset.price,
+          ...asset,
+        }
+      }))
+      setCrypto(result)
+      setLoading(false)
+    }
+    preload()
+  }, [])
+
+  if  (loading) {
+    return <Spin fullscreen />
+  }
+
     return (<Layout.Sider width="25%" style={siderStyle}>
-              <Card style={{marginBottom: '1rem'}}>
+      {assets.map(asset => (<Card style={{marginBottom: '1rem'}}>
                 <Statistic           
                 title="Active"
                 value={11.28}
@@ -17,17 +58,15 @@ export default function AppSider() {
                 }}
                 prefix={<ArrowUpOutlined />}
                 suffix="%"/>
-              </Card>
-              <Card>
-                <Statistic           
-                title="Idle"
-                value={9.3}
-                precision={2}
-                valueStyle={{
-                    color: '#cf1322',
-                }}
-                prefix={<ArrowDownOutlined />}
-                suffix="%"/>
-              </Card>
+                <List
+                size='small'
+                dataSource={data}
+                renderItem={(item) => (
+                  <List.Item>
+                    <Typography.Text mark>[ITEM]</Typography.Text> {item}
+                  </List.Item>
+              )}
+            />
+              </Card>))}
             </Layout.Sider>)
 }

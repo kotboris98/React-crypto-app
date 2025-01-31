566cca83429079f36232b309de0e25cb1582f2e2
kotboris98 <borisvip169@gmail.com>
2025-02-01 01:35:52 +0300

    Initial commit

--- react-crypto-app/src/components/layout/AppSider.jsx
+++ react-crypto-app/src/components/layout/AppSider.jsx
@@ -1,20 +1,12 @@
-import { Layout, Card, Statistic, List, Typography, Spin } from 'antd';
+import { Layout, Card, Statistic, List, Typography, Spin, Tag } from 'antd';
 import { ArrowDownOutlined, ArrowUpOutlined } from '@ant-design/icons';
 import { useEffect, useState } from 'react';
 import { fakeFetchCrypto, fetchAssets } from '../../api';
-import { percentDifference } from '../../utils';
+import { percentDifference, capitalize } from '../../utils';
 
 const siderStyle = {
     padding: '1rem',
   };
-
-  const data = [
-    'Racing car sprays burning fuel into crowd.',
-    'Japanese princess to wed commoner.',
-    'Australian walks 100km after outback crash.',
-    'Man charged over missing wedding girl.',
-    'Los Angeles battles huge wildfires.',
-  ];
 
 export default function AppSider() {
   const [loading, setLoading] = useState(false)
@@ -48,25 +40,42 @@ export default function AppSider() {
   }
 
     return (<Layout.Sider width="25%" style={siderStyle}>
-      {assets.map(asset => (<Card style={{marginBottom: '1rem'}}>
+      {assets.map(asset => (<Card key={asset.id} style={{marginBottom: '1rem'}}>
                 <Statistic           
-                title="Active"
-                value={11.28}
+                title={capitalize(asset.id)}
+                value={asset.totalAmount}
                 precision={2}
-                valueStyle={{
-                    color: '#3f8600',
-                }}
-                prefix={<ArrowUpOutlined />}
-                suffix="%"/>
+                valueStyle={{ color: asset.grow ? '#3f8600': '#cf1322' }}
+                prefix={asset.grow ? <ArrowUpOutlined /> : <ArrowDownOutlined />}
+                suffix="$"/>
                 <List
                 size='small'
-                dataSource={data}
+                dataSource={[
+                  {
+                    title: 'Total profit', 
+                    value: asset.totalProfit,
+                    withTag: true,
+                  },
+                  {title: 'Asset amount', value: asset.amount, isPlain : true},
+                  //{title: 'Difference', value: asset.growPercent},
+                ]}
                 renderItem={(item) => (
                   <List.Item>
-                    <Typography.Text mark>[ITEM]</Typography.Text> {item}
+                    <span>{item.title}</span>
+                    <span>
+                    {item.withTag && 
+                    <Tag color = {asset.grow ? 'green' : 'red'}>
+                      {asset.growPercent}%
+                    </Tag>}
+                    {item.isPlain && item.value}
+                    {!item.isPlain && (
+                      <Typography.Text type={asset.grow ? 'success' : 'danger'}>
+                      {item.value.toFixed(2)}
+                      </Typography.Text>)}
+                    </span>
                   </List.Item>
               )}
-            />
+              />
               </Card>))}
             </Layout.Sider>)
 }

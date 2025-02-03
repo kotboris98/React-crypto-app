2d0fceaeb98849e9c39ca60d0d60e9bda23b2c0a
kotboris98 <borisvip169@gmail.com>
2025-02-03 17:04:41 +0300

    Initial commit

--- react-crypto-app/src/components/layout/AppHeader.jsx
+++ react-crypto-app/src/components/layout/AppHeader.jsx
@@ -1,5 +1,6 @@
-import { Layout, Select, Space, Button } from 'antd';
+import { Layout, Select, Space, Button, Modal } from 'antd';
 import { useCrypto } from '../../context/crypto-context';
+import { useEffect, useState } from 'react';
 
 const headerStyle = {
     width: '100%',
@@ -10,45 +11,34 @@ const headerStyle = {
     justifyContent: 'space-between',
     alignItems: 'center',
   };
-
-  const handleChange = (value) => {
-    console.log(`selected ${value}`);
-  };
-  const options = [
-    {
-      label: 'China',
-      value: 'china',
-      emoji: '🇨🇳',
-      desc: 'China (中国)',
-    },
-    {
-      label: 'USA',
-      value: 'usa',
-      emoji: '🇺🇸',
-      desc: 'USA (美国)',
-    },
-    {
-      label: 'Japan',
-      value: 'japan',
-      emoji: '🇯🇵',
-      desc: 'Japan (日本)',
-    },
-    {
-      label: 'Korea',
-      value: 'korea',
-      emoji: '🇰🇷',
-      desc: 'Korea (韩国)',
-    },
-  ];
   
   export default function AppHeader() {
+    const [select, setSelect] = useState(false)
+    const [modal, setModal] = useState(false)
     const {crypto} = useCrypto()
+
+    useEffect(() => {
+      const keypress = (event) => {
+        if (event.key === '/') {
+          setSelect((prev) => !prev)
+        }
+      }
+      document.addEventListener('keypress', keypress)
+      return () => document.removeEventListener('keypress', keypress)
+    }, [])
+
+    function handleSelect(value) {
+      setModal(true)
+    }
 
     return (<Layout.Header style={headerStyle}>
      <Select
     style={{
       width: 250,
     }}
+    open={select}
+    onSelect={handleSelect}
+    onClick={() => setSelect((prev) => !prev)}
     value="press / to open"
     optionLabelProp='label'
     options={crypto.map(coin => ({
@@ -58,10 +48,20 @@ const headerStyle = {
     }))}
     optionRender={(option) => (
       <Space>
-        <img src = {option.data.icon} /> {option.data.label}
+        <img style = {{width: 20}}
+        src = {option.data.icon} /> 
+        {option.data.label}
       </Space>
     )}
   />  
       <Button type="primary">Add asset</Button>
+      <Modal 
+      open={modal} 
+      onOk={() => setModal(false)} 
+      onCancel={() => setModal(false)}>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Modal>
     </Layout.Header>)
 }
